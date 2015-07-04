bae7ce9ee77ebe5a084648d4b09c1e2401796b06
hesicong <derekhe@april1985.com>
2015-07-05 00:51:22 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,11 +4,13 @@ chmod +x *.sh *.py
 sudo patch -b /boot/config.txt 7inch.patch
 sudo apt-get install -y python3-pip libudev-dev
 sudo pip-3.2 install python-uinput pyudev
+#if pip-3.2 can't be found, please use pip3
+#sudo pip3 install python-uinput pyudev
 
 sudo cp touch.py /usr/bin/
 sudo cp touch.sh /etc/init.d/
 
-chmod +x /usr/bin/touch.py
-chmod +x /etc/init.d/touch.sh
+sudo chmod +x /usr/bin/touch.py
+sudo chmod +x /etc/init.d/touch.sh
 
 sudo update-rc.d touch.sh defaults
